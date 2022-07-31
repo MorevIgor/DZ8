aedee13154e279b2a5fdb0e2780c1b3ea00344ba
Morev Igor <mo.ig2017@yandex.ru>
2022-07-31 09:22:43 +0500

    Creat program

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поля для паспорта
+    public String patronymic; //поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для согласия на рассылку
+
+    FormDate birthday = new FormDate();
+
+
+}
